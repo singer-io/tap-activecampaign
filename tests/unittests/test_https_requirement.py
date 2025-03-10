bedfd81018a99da50812848e4ef5edff212d9da0
Collin Simon <csimon@talend.com>
2025-03-10 19:17:14 +0000

    Add tests to enforce the HTTPS requirements

---
+++ tests/unittests/test_https_requirement.py
@@ -0,0 +1,15 @@
+from tap_activecampaign.client import ActiveCampaignClient
+import unittest
+
+
+class TestRequestHTTPSRequirement(unittest.TestCase):
+    '''
+    Test that the client raises an Exception when the api_url is not https
+    '''
+    def test_config_provided_request_timeout(self):
+        """
+            Unit tests to ensure that request timeout is set based on config value
+        """
+        config = {"api_url": "http://dummy_url.com", "api_token": "dummy_at", "user_agent": "test_ua", "request_timeout": 100}
+
+        self.assertRaises(Exception, ActiveCampaignClient(**config))
