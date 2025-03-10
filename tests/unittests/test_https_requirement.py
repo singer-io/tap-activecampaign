a484b2294cf88beab3321368c8c94731bce997d8
Collin Simon <csimon@talend.com>
2025-03-10 19:35:10 +0000

    Update raisesAssert to latest syntax

--- tests/unittests/test_https_requirement.py
+++ tests/unittests/test_https_requirement.py
@@ -3,13 +3,10 @@ import unittest
 
 
 class TestRequestHTTPSRequirement(unittest.TestCase):
-    '''
-    Test that the client raises an Exception when the api_url is not https
-    '''
     def test_https_requirement(self):
-        """
-            Unit tests to ensure that request timeout is set based on config value
-        """
+        '''
+        Test that the client raises an Exception when the api_url is not https
+        '''
         config = {"api_url": "http://dummy_url.com", "api_token": "dummy_at", "user_agent": "test_ua", "request_timeout": 100}
-
-        self.assertRaises(Exception, ActiveCampaignClient(**config))
+        with self.assertRaises(Exception):
+            ActiveCampaignClient(**config)
