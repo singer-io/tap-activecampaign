69220d030f37f417e54d39aff1f18db1e72ef2fd
Andy Lu <andy@stitchdata.com>
2025-08-27 14:02:01 +0000

    Swap `pyhumps` for `camel-converter`
    This reverts 715b71d, 9d3623d

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='tap-activecampaign',
       py_modules=['tap_activecampaign'],
       install_requires=[
           'backoff==1.10.0',
+          'camel-converter==4.0.1',
           'requests==2.32.4',
           'singer-python==5.13.2'
       ],
